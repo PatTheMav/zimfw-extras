307199fe0419afb8865f22512b5b50de4347c94b
PatTheMav <PatTheMav@users.noreply.github.com>
2024-03-27 14:03:08 +0100

    Update Zsh precmd function for macOS Terminal.app

--- macos_extras.zsh
+++ macos_extras.zsh
@@ -1,23 +1,27 @@
 if [[ -z "$ZEXT_OS" ]]; then
-    export HOMEBREW_INSTALL_BADGE="🖥 "
+    export HOMEBREW_INSTALL_BADGE="💻 "
 
-    if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] && ! typeset -f update_terminal_cwd > /dev/null; then
-        urlencode() {
-            emulate -L zsh
+    if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
+        termtitle_macos() {
             setopt localoptions extendedglob
 
-            input=( ${(s::)1} )
-            print "${(j::)input/(#b)([^A-Za-z0-9_.\!~*\'\(\)-\/])/%${(l:2::0:)$(([##16]#match))}}"
+            local -a input=(${(s::)${PWD}})
+            local pwd_encoded="${(j::)input/(#b)([^A-Za-z0-9_.\!~*\'\(\)-\/])/%${(l:2::0:)$(([##16]#match))}}"
+
+            printf '\e]7;%s\a' "file://${HOST}${pwd_encoded}"
         }
 
-        termtitle_macos() {
-            local pwd_enc=$(urlencode "$(pwd)")
-            printf '\e]7;%s\a' "file://${HOST}${pwd_enc}"
-        }
+        autoload -Uz add-zsh-hook
 
-        autoload -Uz add-zsh-hook && add-zsh-hook precmd termtitle_macos
+        add-zsh-hook precmd termtitle_macos
+
+        if typeset -f update_terminal_cwd > /dev/null; then
+            add-zsh-hook -d precmd update_terminal_cwd
+            unfunction update_terminal_cwd
+        fi
+
         termtitle_macos
-    fi
+    }
 
     alias ps2='ps -facx '
     alias ps1='ps -faxo "user etime pid args" '
